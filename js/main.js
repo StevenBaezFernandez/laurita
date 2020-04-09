236ca649353e598c960f41685135971ae0a5bfb4
Steven <stevenbaezfernandez@gmail.com>
2020-04-09 14:01:13 -0400

    footer y comentario

--- js/main.js
+++ js/main.js
@@ -9,14 +9,24 @@ const menu_scroll = document.querySelector('.menu_scroll');
 const btn_ir_arriba = document.querySelector('.btn_ir_arriba');
 let html = document.getElementsByTagName("html")[0];
 let body = document.getElementsByTagName("body")[0];
+const info_img1 = document.querySelector(".info_img1");
+const info_img2 =document.querySelector(".info_img2");
+const info_img3 =document.querySelector(".info_img3");
+const info_container = document.querySelector(".info_container");
 
 window.addEventListener("load", ()=>{
     setInterval(() => {
         document.querySelector(".blanco").classList.add("active");
-        document.querySelector(".info_img1").classList.add("active");
-        document.querySelector(".info_img2").classList.add("active");
-        document.querySelector(".info_img3").classList.add("active");
-        document.querySelector(".info_container").classList.add("active");
+        if(info_img1 && info_img2 && info_img3){
+            
+            info_img1.classList.add("active");
+            info_img2.classList.add("active");
+            info_img3.classList.add("active");
+            
+        }
+        if(info_container){
+            info_container.classList.add("active");
+        }
     }, 2000);
     
 });
@@ -53,7 +63,7 @@ btn_ir_arriba.addEventListener("click", ()=>{
 function scrollAnimation(){
     
     document.addEventListener("scroll", ()=>{
-        if(html.scrollTop >=800){
+        if(html.scrollTop >=800 && fashion2_img){
             fashion2_img.classList.add('active');
         }
         if(html.scrollTop >=1450){
